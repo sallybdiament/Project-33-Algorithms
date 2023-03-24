73d96c73dc56b299dc84f8ed31730fc0f4e50894
Sally Diament <sally.barzilai@gmail.com>
2023-03-23 22:14:32 -0300

    returning None when params are not integers or is None

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,3 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    # começando
     raise NotImplementedError
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,7 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    for p in permanence_period:
+        if type(p[0]) != int or type(p[1]) != int:
+            return None
+    
