f88410f52283cab4c06bd58ba7d2f01ec509bd70
Sally Diament <sally.barzilai@gmail.com>
2023-03-24 20:11:37 -0300

    including method study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,20 @@
+# [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)]
+
+# target_time de 1 a 7.
+# target_time = 5 ---> resultado: 3
+
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
     for p in permanence_period:
         if type(p[0]) != int or type(p[1]) != int:
             return None
+
+    if target_time is None:
+        return None
+    
+    quantity_students = 0
+    for p in permanence_period:
+        if p[0] <= target_time and target_time <= p[1]:
+            quantity_students += 1
+    
+    return quantity_students
     
