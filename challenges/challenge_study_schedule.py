9268b3832692733fa686ce8b4a39cc569039f523
Sally Diament <sally.barzilai@gmail.com>
2023-03-24 22:01:04 -0300

    flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,8 @@
-# [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)]
-
-# target_time de 1 a 7.
-# target_time = 5 ---> resultado: 3
-
 def study_schedule(permanence_period, target_time):
     for p in permanence_period:
         if type(p[0]) != int or type(p[1]) != int:
             return None
-        
+ 
     if target_time is None:
         return None
 
