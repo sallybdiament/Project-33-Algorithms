c47ea49e2d108b4729254e5e13e222a1b2fe63eb
Sally Diament <sally.barzilai@gmail.com>
2023-03-24 21:58:58 -0300

    flake8

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,6 +4,6 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index >= high_index:
         return True
     elif word[low_index] == word[high_index]:
-        return is_palindrome_recursive(word, low_index + 1, high_index -1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     else:
         return False
