38cfefea14a0d57eae6a4583966c7262b9e5a0ac
Sally Diament <sally.barzilai@gmail.com>
2023-03-24 21:56:34 -0300

    flake8 correctiong

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -7,14 +7,13 @@ def study_schedule(permanence_period, target_time):
     for p in permanence_period:
         if type(p[0]) != int or type(p[1]) != int:
             return None
-
+        
     if target_time is None:
         return None
-    
+
     quantity_students = 0
     for p in permanence_period:
         if p[0] <= target_time and target_time <= p[1]:
             quantity_students += 1
-    
+
     return quantity_students
-    
